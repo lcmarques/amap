deac7a254f7005c727a3b9d70a5fc8edff98024c
Luís Marques <lcarapinha@gmail.com>
2013-03-03 23:43:34 +0000

    Version 0.2
    - New output formatting (table-like)
    - New options for list option (all diskgroups or a specified diskgroup)
    - New feature: Check free and used allocation units for a specified
    diskgroup or all diskgroups.
    - Bug fixes
    - New logo   and credits for Luca Canali ASM internals work.

--- amap.sql
+++ amap.sql
@@ -1,16 +1,111 @@
-set serveroutput on
+--
+--
+--    _     _      _     _      _     _      _     _   
+--   (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c)  
+--    / ._. \      / ._. \      / ._. \      / ._. \   
+--  __\( Y )/__  __\( Y )/__  __\( Y )/__  __\( Y )/__ 
+-- (_.-/'-'\-._)(_.-/'-'\-._)(_.-/'-'\-._)(_.-/'-'\-._)
+--    || A ||      || M ||      || A ||      || P ||   
+--  _.' `-' '._  _.' `-' '._  _.' `-' '._  _.' `-' '._ 
+-- (.-./`-'\.-.)(.-./`-'\.-.)(.-./`-'\.-.)(.-./`-'\.-.)
+--  `-'     `-'  `-'     `-'  `-'     `-'  `-'     `-' 
+-- AMAP - ASM Mapping Utility v0.2 
+-- Author: Luis Marques (lcarapinha@gmail)
+-- Website: http://lcmarques.com
+-- Twitter: @drune
+-- Based on Lucas Canali work on ASM internals
+
+-- USAGE: @amap list DGNAME
+--        @amap free DGNAME
+-- 	  @amap ASMFILENAME
+
+
+set serveroutput on format wrapped
 SET VERIFY OFF
+SET FEEDBACK OFF
+set pagesize 0
+set linesize 9999
+
 
 declare
-getops   varchar2(100);
+getops   varchar2(50) := '';
+getops2  varchar(50) := '';
 
 begin
 
 getops := '&1';
+getops2 := '&2';
 
-if (getops = 'list') then
+if (getops = 'free') then
+	 
+          DBMS_OUTPUT.put_line('     DISKGROUP       DISKNUMBER AU STATUS   AU COUNT  ');
+  	  DBMS_OUTPUT.put_line('-------------------- ---------- --------- ------------');
 
-dbms_output.put_line('== ASM FILE ALIAS LIST ==');
+	  if (getops2 = '*') then
+
+		FOR free_au IN (
+		select a.name, number_kfdat, decode(v_kfdat, 'V', 'USED AU', 'FREE AU') as status_au, count(*) as num_au
+			from x$kfdat d, v$asm_diskgroup a
+			where d.group_kfdat = a.group_number
+			group by a.name, number_kfdat, v_kfdat)
+
+
+			LOOP
+    dbms_output.put_line(lpad(free_au.name, 20) || ' ' ||  rpad(free_au.number_kfdat, 10)  || ' ' ||  rpad(free_au.status_au,9) || ' ' || rpad(free_au.num_au,12));
+        		END LOOP;
+	else
+		FOR free_au IN (
+                select a.name, number_kfdat, decode(v_kfdat, 'V','USED AU','FREE AU') as status_au , count(*) as num_au
+                        from x$kfdat d, v$asm_diskgroup a
+                        where d.group_kfdat = a.group_number
+			and a.name = getops2
+                        group by a.name, number_kfdat, v_kfdat)
+
+	
+			LOOP
+
+  dbms_output.put_line(lpad(free_au.name, 20) || ' ' ||  rpad(free_au.number_kfdat, 10)  || ' ' ||  rpad(free_au.status_au,9) || ' ' || rpad(free_au.num_au,12));
+			END LOOP;  
+
+	END IF;
+
+elsif (getops = 'list')  then
+	  
+	  DBMS_OUTPUT.put_line('     ASM FILE NAME                   DISKGROUP      FILENUMBER GROUPNUMBER');
+          DBMS_OUTPUT.put_line('------------------------------ -------------------- ---------- ------------');
+
+
+	if (getops2 = '*') then
+
+	FOR asm_files IN (
+		SELECT file_number, aname, dir, gname, group_number   
+		FROM (
+ 			SELECT file_number, aname, dir, gname, group_number 
+ 			FROM (
+			  SELECT g.name gname, 
+			   a.group_number,
+			   a.parent_index pindex,
+			   a.file_number, 
+			   a.name aname,
+			   a.reference_index rindex, 
+			   a.alias_directory dir, 
+ 			  a.system_created sys 
+			  FROM v$asm_alias a, v$asm_diskgroup g 
+ 			 WHERE a.group_number = g.group_number
+ 				 )
+		 where dir = 'N'
+ 			START WITH (mod(pindex, power(2, 24))) = 0
+ CONNECT BY PRIOR rindex = pindex
+ ORDER BY dir desc)
+)
+LOOP
+
+
+dbms_output.put_line(lpad(asm_files.aname, 30) || ' ' ||  rpad(asm_files.gname, 20)  || ' ' ||  rpad(asm_files.file_number,10) || ' ' || rpad(asm_files.group_number,12));
+
+END LOOP; 
+
+ELSE 
 
 FOR asm_files IN (
 SELECT file_number, aname, dir, gname, group_number   
@@ -26,33 +121,40 @@ FROM (
    a.alias_directory dir, 
    a.system_created sys 
   FROM v$asm_alias a, v$asm_diskgroup g 
-  WHERE a.group_number = g.group_number
+  WHERE a.group_number = g.group_number 
   )
  where dir = 'N'
+ and gname = getops2
  START WITH (mod(pindex, power(2, 24))) = 0
  CONNECT BY PRIOR rindex = pindex
  ORDER BY dir desc)
 )
+
 LOOP
+dbms_output.put_line(lpad(asm_files.aname, 30) || ' ' ||  rpad(asm_files.gname, 20)  || ' ' ||  rpad(asm_files.file_number,10) || ' ' || rpad(asm_files.group_number,12));
 
-dbms_output.put_line('NAME: ' || asm_files.aname || '  [DG=+'||asm_files.gname||';FN='||asm_files.file_number||';GN='|| asm_files.group_number || ']');
+END LOOP;
 
-END LOOP; 
+END IF;
+
 
 ELSE
 
-
-dbms_output.put_line('[GROUP_NUMBER ; DISKNUMBER ; FILENUMBER ; FILE_EXTENT_NUMBER ; RELATIVE_AU_POS]'); 
+DBMS_OUTPUT.put_line('GROUPNUMBER DISKNUMBER FILENUMBER FILE EXTENT NUMBER EXTENT MIRRORING RELATIVE AU POSITION');
+DBMS_OUTPUT.put_line('----------- ---------- ---------- ------------------ ---------------- --------------------');
+DBMS_OUTPUT.ENABLE(200000);
 
 for asm_ext IN (
-select distinct GROUP_KFFXP,DISK_KFFXP,NUMBER_KFFXP,XNUM_KFFXP,DECODE(LXN_KFFXP, '0', 'PRIMARY EXTENT', '1', 'MIRROR EXTENT','2', '2ND MIRROR EXTENT') EXTP, AU_KFFXP  
+select distinct GROUP_KFFXP,DISK_KFFXP,NUMBER_KFFXP,XNUM_KFFXP,DECODE(LXN_KFFXP, '0', 'PRIMARY EXT', '1', '1st MIRROR EXT','2', '2nd MIRROR EXT') EXTP, AU_KFFXP  
 from x$kffxp
 where number_kffxp=(select file_number from v$asm_alias where name=getops)     
 and GROUP_KFFXP=(select group_number from v$asm_alias where name=getops)           
 order by NUMBER_KFFXP, AU_KFFXP)
 
 LOOP
-dbms_output.put_line('[' || asm_ext.GROUP_KFFXP || ' ; ' || asm_ext.DISK_KFFXP || ' ; ' || asm_ext.NUMBER_KFFXP || ' ; ' || asm_ext.XNUM_KFFXP || ' ; ' || asm_ext.EXTP || ' ; ' || asm_ext.AU_KFFXP || ']');
+
+dbms_output.put_line(lpad(asm_ext.GROUP_KFFXP, 11) || ' ' ||  rpad(asm_ext.DISK_KFFXP, 10)  || ' ' ||  rpad(asm_ext.NUMBER_KFFXP,10) || ' ' || rpad(asm_ext.XNUM_KFFXP,18) || rpad(asm_ext.extp,16) || ' ' || rpad(asm_ext.au_kffxp,20));
+
 
 END LOOP;
 
